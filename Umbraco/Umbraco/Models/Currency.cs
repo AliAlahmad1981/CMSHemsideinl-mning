3c728e34150a1ee86b40a891f96da07eedd7d748
Ali <Ali@DESKTOP-SHBG8HS>
2023-03-11 10:43:24 +0200

    adding services

---
+++ Umbraco/Umbraco/Models/Currency.cs
@@ -0,0 +1,6 @@
+﻿namespace Umbraco.Models
+{
+    public class Currency
+    {
+    }
+}
---
+++ Umbraco/Umbraco/Services/CurrencyService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+using Umbraco.Models;
+
+namespace Umbraco.Services
+{
+    public class CurrencyService
+    {
+
+
+        public async ValueTask<Currency> GetAllCurrenciesAsync()
+        {// f8anUVQRcqa5EME30o43EbmV4d1hjiH8tsdstjj
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // await httpClient.GetFromJsonAsync("");
+            }
+            return await ValueTask.FromResult(new Currency());
+        }
+    }
+}
